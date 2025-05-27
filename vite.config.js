f6959408cb78112b734dd3ca8ed8a8b5d1a43765
Ricko2005 <romarictolofon7@gmail.com>
2025-05-27 08:54:43 +0100

    Ajout de soutenance-5.onrender.com dans allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,10 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: '0.0.0.0',       // Permet à l'application d'écouter sur toutes les interfaces réseau
-        port: process.env.PORT || 3000,  // Utilise la variable d'environnement PORT si définie
+        host: '0.0.0.0', // Écouter sur toutes les interfaces réseau
+        port: process.env.PORT || 3000, // Utilise la variable d'environnement PORT si définie
         preview: {
-            allowedHosts: ['soutenance-3.onrender.com'],  // Ajoute l'hôte autorisé
+            allowedHosts: ['soutenance-5.onrender.com', 'localhost'], // Ajoute l'hôte autorisé
         },
     },
 });
